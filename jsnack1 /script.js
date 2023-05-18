9105c6eb7b3465ad1f24ac38687bd2612c6044ca
EmanuelaPau <uapame@gmail.com>
2023-05-18 15:03:56 +0200

    Add structure of the problem

--- jsnack1 /script.js
+++ jsnack1 /script.js
@@ -1,1 +1,22 @@
 console.log("Hello!");
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+// 1. chiedo per 10 volte il numero attraverso il prompt 
+    // 1.1 creo un array dove storare i numeri
+    // 1.2 creo un ciclo for che duri 10 volte
+    // 1.3 dentro al ciclo for aggiungo un prompt dove chiedo all'utente un numero
+    // 1.4 salvo quel numero dentro alla mia array
+
+
+// 2. sommo i numeri che mi ha dato
+    // 2.0 creo una variabile somma
+    // 2.1 creo un ciclo for che duri 10 volte
+    // 2.2 prendo il numero dentro al mio array corrispondente al numero del ciclo in cui sono
+    // 2.3 lo sommo alla variabile somma
+
+
+// 3. stampo la somma 
+    //3.1 richiamo la mia variabile somma
