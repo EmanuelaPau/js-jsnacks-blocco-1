0c5a5706fc5b6a0ca0b184b455c809fa1a3ff3b6
EmanuelaPau <uapame@gmail.com>
2023-05-18 15:05:04 +0200

    Add numbers list array

--- jsnack1 /script.js
+++ jsnack1 /script.js
@@ -5,18 +5,21 @@ console.log("Hello!");
 
 
 // 1. chiedo per 10 volte il numero attraverso il prompt 
-    // 1.1 creo un array dove storare i numeri
-    // 1.2 creo un ciclo for che duri 10 volte
-    // 1.3 dentro al ciclo for aggiungo un prompt dove chiedo all'utente un numero
-    // 1.4 salvo quel numero dentro alla mia array
+// 1.1 creo un array dove storare i numeri
+// 1.2 creo un ciclo for che duri 10 volte
+// 1.3 dentro al ciclo for aggiungo un prompt dove chiedo all'utente un numero
+// 1.4 salvo quel numero dentro alla mia array
 
 
 // 2. sommo i numeri che mi ha dato
-    // 2.0 creo una variabile somma
-    // 2.1 creo un ciclo for che duri 10 volte
-    // 2.2 prendo il numero dentro al mio array corrispondente al numero del ciclo in cui sono
-    // 2.3 lo sommo alla variabile somma
+// 2.0 creo una variabile somma
+// 2.1 creo un ciclo for che duri 10 volte
+// 2.2 prendo il numero dentro al mio array corrispondente al numero del ciclo in cui sono
+// 2.3 lo sommo alla variabile somma
 
 
 // 3. stampo la somma 
-    //3.1 richiamo la mia variabile somma
+//3.1 richiamo la mia variabile somma
+
+let numbersList = ["pippo", "paperina"];
+console.log(numbersList);
