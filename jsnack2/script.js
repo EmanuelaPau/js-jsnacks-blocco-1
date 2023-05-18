1bf8ecb9d55fa6d221b803d4ee395ba47b4c7f4a
EmanuelaPau <uapame@gmail.com>
2023-05-18 15:27:51 +0200

    S2 - Add problem structure

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,14 @@
-console.log("Hello!");
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+console.log("Hello! Snack 2. Can you enter the party?");
+
+// Aggiungo un array con i nomi degli invitati
+// Chiedo all'utente il nome attraverso un array e la salvo in una variabile nome
+// creo una variabile booleana di verifica e la imposto false 
+// creo un ciclo for che ha durata pari alla lunghezza dell'array
+// dentro al ciclo confronto il nome dell'utente con i nomi della lista
+// se il nome corrisponde trasformo la variabile di verifica in true 
+// esco dal ciclo for 
+// se la variabile e' true stampo un puo' entrare 
+// se e' false stampo non puo entrare 
+
