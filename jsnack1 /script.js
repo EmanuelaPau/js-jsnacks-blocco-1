0a5772c7eb84e77d4f29eb6b2db7b72165ad9153
EmanuelaPau <uapame@gmail.com>
2023-05-18 15:11:05 +0200

    Add parseInt

--- jsnack1 /script.js
+++ jsnack1 /script.js
@@ -24,8 +24,8 @@ console.log("Hello!");
 let numbersList = [];
 
 for (let i = 0; i < 10; i++) {
-    let myNumber = prompt(`${i} tell me a number`);
-    numbersList.push(myNumber);
+    let myNumber = ;
+    numbersList.push(parseInt(myNumber));
 }
 
 console.log(numbersList);
