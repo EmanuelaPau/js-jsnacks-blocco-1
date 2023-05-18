edc14986630662298fbc625fd2316ba0b119cf7e
EmanuelaPau <uapame@gmail.com>
2023-05-18 15:58:31 +0200

    S3 - add structure and array

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,13 @@
 console.log("Hello!");
+
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,  se è dispari inseriscilo nell’array.
+
+// creo un array 
+// creo un ciclo for che dura 6 volte 
+// creo un prompt legato a una variabile dove chiedo all'utente un numero 
+// creo un if
+// se il modulo di 2 del numero === a 0 allora e' pari e allora aggiungo il numero all'array (array.push)
+// stampo l'array per verifica
+
+let numeriPari = []
