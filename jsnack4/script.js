0dc5aa80b72a0cad35f0715a6bf58368c31fcca4
EmanuelaPau <uapame@gmail.com>
2023-05-18 16:19:48 +0200

    S3 addproblem structure

---
+++ jsnack4/script.js
@@ -0,0 +1,10 @@
+console.log("Hello!");
+
+// Chiedi un numero di 4 cifre all’utente
+// e calcola la somma di tutte le cifre che compongono il numero.
+
+// aggiungi una variabile e chiedi con il prompt un numero di 4 cifre 
+// verifica che il numero sia di 4 cifre
+    // se la variabile.lenght === 4, allora prosegui. se no manda errore e richiedi
+// prendi singole cifre ???? 
+// somma le cifre 
